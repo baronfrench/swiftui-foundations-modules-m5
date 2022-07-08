f7158343bfa88d211173ee7f1324803d4cfacb42
Baron French <baron.french@me.com>
2022-07-08 17:33:17 -0400

    Add assets Add data and parse into ContentModel Add style and parse into ContentModel Add modles

--- LearningApp/View Models/ContentModel.swift
+++ LearningApp/View Models/ContentModel.swift
@@ -9,4 +9,42 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // Gte file URL
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        // Read json
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+        
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+        
+            // assign to local variable
+            self.modules = modules
+            
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+    
+    let styleUrl = Bundle.main.url(forResource: "tyle", withExtension: "html")
+    
+    do {
+        let styleData = try Data(contentsOf: styleUrl)
+        self.styleData = styleData
+    }
+    
 }
