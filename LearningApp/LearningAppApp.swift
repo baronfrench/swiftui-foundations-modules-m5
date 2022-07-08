62b3d4d101a84ee590e37c8b46f3ce61f743078b
Baron French <baron.french@me.com>
2022-07-08 16:50:39 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Baron French on 2022-07-08.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
